717855a72e0b0f3f26fb1e4a5eb7d3e276b11360
Matthew Berryman <matthew@acrossthecloud.net>
2020-09-25 08:53:48 +0000

    handle some exceptions

--- bot.py
+++ bot.py
@@ -54,25 +54,33 @@ def check_mentions(api, since_id):
     new_since_id = max(tweet.id, new_since_id)
     if tweet.in_reply_to_status_id is not None:
       continue
-    search_object=year_re.search(tweet.text)
-    if search_object is not None:
-      year = int(search_object.group(0))
+    try:
+      search_object=year_re.search(tweet.text)
+      if search_object is not None:
+        year = int(search_object.group(0))
 
-      logger.info(f"Answering to {tweet.user.name} for year {year}")
+        logger.info(f"Answering to {tweet.user.name} for year {year}")
 
-      if year >= 2018 or year < 1751:
-        api.update_status(
-          status="Please enter a year from 1751 up to 2017. " + str(round(time.time(),0)),
-          in_reply_to_status_id=tweet.id,
-          auto_populate_reply_metadata=True
-        )
-      else:
-        api.update_status(
-          status="Global % of human CO2 emitted from " + str(year) + " to 2018 = "+str(round(world.loc[world['year']==year]['percent_after'].values[0],2))+"%\n" + \
-          "CO2 concentration that year = "+ str(ppm.loc[ppm['year']==year]['ppm'].values[0]) +"ppm",
-          in_reply_to_status_id=tweet.id,
-          auto_populate_reply_metadata=True
-        )
+        if year >= 2018 or year < 1751:
+          api.update_status(
+            status="Please enter a year from 1751 up to 2017. " + str(round(time.time(),0)),
+            in_reply_to_status_id=tweet.id,
+            auto_populate_reply_metadata=True
+          )
+        else:
+          co2_conc = ""
+          try: 
+            co2_conc = str(ppm.loc[ppm['year']==year]['ppm'].values[0])
+          except IndexError:
+            co2_conc = "(data not available for that year)"
+          api.update_status(
+            status="Global % of human CO2 emitted from " + str(year) + " to 2018 = "+str(round(world.loc[world['year']==year]['percent_after'].values[0],2))+"%\n" + \
+            "CO2 concentration that year = "+ co2_conc +"ppm",
+            in_reply_to_status_id=tweet.id,
+            auto_populate_reply_metadata=True
+          )
+    except Exception:
+      logger.error("Fatal error in main loop", exc_info=True)
   return new_since_id
 
 def main():
